b81a7d4c4dea2c1bb52bd91a913e94504adf76c8
rasmus <mail@rasmusantons.de>
2022-06-25 12:07:04 +0200

    fix image clue selection, actually

--- tasks.py
+++ tasks.py
@@ -26,13 +26,13 @@ def load_tasks(game_type, n_tasks, one_image=False):
                 unused_tasks.append(task)
         if image_tasks:
             image_task = random.choice(image_tasks)
-    n_normal_tasks = n_tasks if image_task else n_tasks - 1
+    n_normal_tasks = n_tasks if image_task is None else (n_tasks - 1)
     chosen_tasks = [(i, available_tasks[i]) for i in random.sample(unused_tasks, k=min(n_normal_tasks, len(unused_tasks)))]
     if image_task:
         chosen_tasks.append((image_task, available_tasks[image_task]))
         random.shuffle(chosen_tasks)
     if len(chosen_tasks) < n_tasks:
-        chosen_tasks.extend([None] * (n_tasks - len(unused_tasks)))
+        chosen_tasks.extend([None] * (n_tasks - len(chosen_tasks)))
     return chosen_tasks
 
 
