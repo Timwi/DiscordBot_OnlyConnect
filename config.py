867dfcbc297f41271426ff289bd65d68f5209662
Timwi <github@timwi.de>
2022-12-17 04:49:28 +0100

    Update quiz data

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
+# TESTING
+
 bot_token = "MTA0MzIxMDg3NzkxMTk2OTgxMg.GcGQbz.fKQje7a9eM0kkFpVk2FtJsMU9HJi3tetdSLl_I"
 guild_id = 1043195236786122833
 admins = [175298987086053376]
 t_connection = 60
+
+
+## LIVE
+#
+#bot_token = "OTIyMTA3NDUxMjcxMTU1NzUz.GId3d-.33N-S198opvrOsxFS5_3p9KIPwBMJ5OtO8iSiE"
+#guild_id = 773319828295385108
+#admins = [175298987086053376]
+#t_connection = 60
